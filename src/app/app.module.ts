e474d223592cafe484b3a5e7b61bdfb484edfb68
Prabhat Dixit <2002dixtprabhat@gmail.com>
2023-04-17 21:01:54 +0530

    17

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { PropertyComponent } from './property/property.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PropertyComponent
   ],
   imports: [
     BrowserModule,
